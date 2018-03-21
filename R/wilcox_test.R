7e19b7971d1c96d34c03e54ca15a09514533b88d
joereinhardt <joachim.reinhardt@fu-berlin.de>
2018-03-21 08:50:19 +0700

    import functions into package

---
+++ R/wilcox_test.R
@@ -0,0 +1,277 @@
+#' Bayesian First Aid Alternative to the t-test
+#'
+#' Function implements a bayesian alternative to the wilcoxon.test() function.
+#' Written as an addition to the Bayesian First Aid (BFA) package by
+#' Rasmus Bååth (https://github.com/rasmusab). BFA's structure is largely
+#' followed. S3 functions to be implemented as in BFA:
+#' print, plot, summary, model.code, and diagnostics.
+#' Method follows http://andrewgelman.com/2015/07/13/dont-do-the-wilcoxon/, and
+#' fits a normal model using JAGS after rank-transformation.
+#'
+#' @param x numeric vector of data values
+#' @param y numeric vector of data values to be compared to x
+#' @param cred.mass he amount of probability mass that will be contained in
+#'      reported credible intervals. This argument fills a similar role as
+#'      conf.level in \code{\link{wilcox.test}}
+#' @param mu a number specifying an optional parameter used to form the null
+#'     hypothesis.
+#' @param n.iter The number of iterations to run the MCMC sampling.
+#' @param alternative ignored, only retained in order to maintain compability
+#'     with \code{\link{wilcox.test}}
+#' @param conf.level identical to cred.mass,
+#'     ignored, only retained in order to maintain compability with
+#'     \code{\link{wilcox.test}}
+#'
+#' @return A list of class \code{bayes_wilcox_test}. It can be further inspected
+#'  using the functions \code{summary}, \code{plot}, \code{diagnostics}
+#'  and \code{model.code}.
+#'
+#' @import coda
+#' @import rjags
+#' @import stats
+#' @import graphics
+#' @import grDevices
+#' @import MASS
+#'
+#' @export
+
+
+#Note: needs to take same input as wilcox.test()
+
+bayes.wilcox.test <- function(x, y, cred.mass = 0.95, mu = 0, n.iter = 30000,
+                              alternative = NULL, conf.level) {
+  #Note: BFA uses code from the "original" function for setting up the test
+
+  #If statements from BFA
+  if (!missing(alternative)) {
+    warning("The argument 'alternative' is ignored by bayes.binom.test")
+  }
+
+  if (!missing(conf.level)) {
+    cred.mass <- conf.level
+  }
+
+  #require necessary packages
+  #require("rjags")
+  #require("BayesianFirstAid")
+  #Note: add rules for only x given, etc
+  #add mu parameter (?)
+  ## Data Transformation
+  data <- c(x, y)
+  #namings: from BFA / default
+  x_name <- deparse(substitute(x))
+  y_name <- deparse(substitute(y))
+  data_name <- paste(x_name, "and", y_name)
+  N <- length(data)
+  quantiles <- seq(from = 1, to = (2 * N - 1), by = 2)/(2*N)
+  transformedRanks <- qnorm(quantiles[rank(data)])
+  tR_x <- transformedRanks[1:length(x)]
+  tR_y <- transformedRanks[-(1:length(y))]
+  d <- tR_x - tR_y
+
+  ### Running Model. Code adapted from BFA
+  mcmc_samples <- jags_wilcox_test(d)
+  stats <- mcmc_stats(mcmc_samples, cred_mass = cred.mass,
+                      comp_val = mu)
+  bfa_object <- list(x = x, y = y, d = d, n = N, comp = mu,
+                     cred_mass = cred.mass, x_name = x_name, y_name = y_name,
+                     data_name = data_name, mcmc_samples = mcmc_samples,
+                     stats = stats)
+  class(bfa_object) <- c("bayes_wilcox_test", "bayesian_first_aid")
+  bfa_object
+}
+
+#JAGS model string
+wilcox_model_string <- "model {
+for (i in 1:length(d)) {
+d[i] ~ dnorm(mu, tau)
+}
+mu ~ dnorm(mean_mu, precision_mu)
+tau <- 1 / pow(sigma, 2)
+sigma ~ dunif(sigma_low, sigma_high)
+}"
+
+#Function for JAGS model
+jags_wilcox_test <- function(d, n.chains = 3, n.update = 500, n.iter = 5000,
+                             n.adapt = 500, thin = 1, progress.bar = "text") {
+  data_list <- list(
+    d = d,
+    mean_mu = mean(d, trim = 0.2),
+    precision_mu = 1 / (mad(d)^2 * 1000000),
+    sigma_low = mad(d) / 1000,
+    sigma_high = mad(d) * 1000
+  )
+  inits_list <- list(mu = mean(d, trim = 0.2), sigma = mad0(d))
+  params <- c("mu", "sigma")
+  mcmc_samples <- run_jags(wilcox_model_string,
+                           data = data_list,
+                           inits = inits_list,
+                           params = params,
+                           n.chains = n.chains,
+                           n.adapt = n.adapt,
+                           n.update = n.update,
+                           n.iter = n.iter,
+                           thin = thin,
+                           progress.bar = progress.bar)
+  mcmc_samples
+
+}
+
+### wilcox test S3 methods ###
+
+print.bayes_wilcox_test <- function(object, ...) {
+  s <- format_stats(object$stats)
+
+  cat("\n")
+  cat("\tBayesian First Aid Wilcoxon test\n")
+  cat("\n")
+  cat("data: ", object$x_name, " (n = ", length(object$x) ,") and ",
+      object$y_name, " (n = ", length(object$y) ,")\n", sep = "")
+  cat("\n")
+  cat("  Estimates [", s[1, "HDI%"] ,"% credible interval]\n", sep = "")
+  cat("difference of the means: ", s["mu", "median"], " [", s["mu", "HDIlo"],
+      ", ", s["mu", "HDIup"] , "]\n",sep = "")
+  cat("\n")
+  cat("The difference of the means is greater than", s["mu","comp"] ,
+      "by a probability of", s["mu","%>comp"], "\n")
+  cat("and less than", s["mu", "comp"] ,
+      "by a probability of", s["mu", "%<comp"], "\n")
+  cat("\n")
+  invisible(NULL)
+}
+
+summary.bayes_wilcox_test <- function(object, ...) {
+  s <- round(object$stats, 3)
+  ##Issues: Is sigma relevant here? x_pred / y_pred (diff_pred)?
+  cat("  Data\n")
+  cat(object$x_name, ", n = ", length(object$x), "\n", sep = "")
+  cat(object$y_name, ", n = ", length(object$y), "\n", sep = "")
+  cat("\n")
+
+  #print_bayes_two_sample_t_test_params(object) #Replace by shorter:
+  cat("  Model parameters and generated quantities\n")
+  cat("mu: the difference in means of ", object$x_name, "and",
+      object$y_name, "\n")
+  cat("sigma: the difference in scale of", object$x_name, "and",
+      object$y_name, "\n")
+
+  cat("\n")
+
+  cat("  Measures\n" )
+  print(s[, c("mean", "sd", "HDIlo", "HDIup", "%<comp", "%>comp")])
+  cat("\n")
+  cat("'HDIlo' and 'HDIup' are the limits of a ", s[1, "HDI%"] ,
+      "% HDI credible interval.\n", sep = "")
+  cat("'%<comp' and '%>comp' are the probabilities of the respective parameter",
+      "being\n")
+  cat("smaller or larger than ", s[1, "comp"] ,".\n", sep = "")
+
+  cat("\n")
+  cat("  Quantiles\n" )
+  print(s[, c("q2.5%", "q25%", "median","q75%", "q97.5%")] )
+  invisible(object$stats)
+}
+
+#adapted from plot.bayes_binom_test
+plot.bayes_wilcox_test <- function(object, ...) {
+  old_par <- par( mar = c(3.5,3.5,2.5,0.5),
+                  mgp = c(2.25,0.7,0),
+                  mfcol = c(1,1))
+  stats <- object$stats
+  mcmc_samples <- object$mcmc_samples
+  samples_mat <- as.matrix(mcmc_samples)
+  mu = samples_mat[,"mu"]
+  sample_mat <- as.matrix(object$mcmc_samples)
+  xlim = range(c(mu, object$comp))
+  plotPost(sample_mat[, "mu"], cred_mass = object$cred_mass,
+           comp_val = object$comp, xlim = xlim, cex = 1, cex.lab = 1.5,
+           main = "Difference in means",
+           xlab = expression(mu_diff), show_median = TRUE)
+  #hist_data <- discrete_hist(sample_mat[, "x_pred"], c(0, x$n),
+  #                           ylab="Probability", x_marked= x$x,
+  #                           xlab = "Number of sucesses",
+  #                           main="Data w. Post. Pred.")
+  #legend("topright", legend="Data", col="red",  lty=1, lwd=3)
+  par(old_par)
+  invisible(NULL)
+}
+
+#Adapted from model.code.bayes_binom_test
+#output code not yet running!
+model.code.bayes_wilcox_test <- function(fit) {
+  cat("### Model code for the Bayesian First Aid alternative to the binomial",
+      "test ###\n\n")
+  cat("require(rjags)\n\n")
+
+  cat("# Setting up the data\n")
+  cat("x <-", fit$x_name, "\n")
+  cat("y <-", fit$y_name, "\n")
+  cat("\n")
+  pretty_print_function_body(wilcox_model_code)
+  invisible(NULL)
+}
+
+# Not to be run, just to be printed - adapted from two_sample_t_test_model_code
+wilcox_model_code <- function(x, y) {
+  # The model string written in the JAGS language
+  BayesianFirstAid::replace_this_with_model_string
+
+  # Setting parameters for the priors that in practice will result
+  # in flat priors on the mu's and sigma's.
+  mean_mu = mean( c(x, y), trim = 0.2)
+  precision_mu = 1 / (mad( c(x, y) )^2 * 1000000)
+  sigma_low = mad( c(x, y) ) / 1000
+  sigma_high = mad( c(x, y) ) * 1000
+
+  # Initializing parameters to sensible starting values helps the convergence
+  # of the MCMC sampling. Here using robust estimates of the mean (trimmed)
+  # and standard deviation (MAD).
+  inits_list <- list(mu = mean(d, trim = 0.2), sigma = mad0(d))
+
+  data_list <- list(
+    d = d,
+    mean_mu = mean(d, trim = 0.2),
+    precision_mu = 1 / (mad(d)^2 * 1000000),
+    sigma_low = mad(d) / 1000,
+    sigma_high = mad(d) * 1000
+  )
+
+  # The parameters to monitor.
+  params <- c("mu", "sigma")
+
+  # Running the model
+  model <- jags.model(textConnection(model_string), data = data_list,
+                      inits = inits_list, n.chains = 3, n.adapt = 1000)
+  update(model, 500) # Burning some samples to the MCMC gods....
+  samples <- coda.samples(model, params, n.iter = 10000)
+
+  # Inspecting the posterior
+  plot(samples)
+  summary(samples)
+}
+
+wilcox_model_code <- inject_model_string(wilcox_model_code,
+                                         wilcox_model_string)
+
+
+#adapted from diagnostics.bayes_two_sample_t_test
+diagnostics.bayes_wilcox_test <- function(fit) {
+  print_mcmc_info(fit$mcmc_samples)
+  cat("\n")
+  print_diagnostics_measures(round(fit$stats, 3))
+  cat("\n")
+  cat("  Model parameters and generated quantities\n")
+  cat("mu: the difference in means of ", fit$x_name, "and",
+      fit$y_name, "\n")
+  cat("sigma: the difference in scale of", fit$x_name, "and",
+      fit$y_name, "\n")
+
+  cat("\n")
+
+  old_par <- par( mar = c(3.5,2.5,2.5,0.5) , mgp = c(2.25,0.7,0) )
+  plot(fit$mcmc_samples)
+  par(old_par)
+  invisible(NULL)
+}
+
+### End wilcox.test S3 functions ###
