6d207d30f626f0ed6066d1088f484a98ed1b1e34
joereinhardt <joachim.reinhardt@fu-berlin.de>
2018-04-18 21:40:33 +0700

    Restructure, write Intro

--- vignettes/BayesianWilcoxTest.R
+++ vignettes/BayesianWilcoxTest.R
@@ -1,7 +1,6 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
-
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
 
