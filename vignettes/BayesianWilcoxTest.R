7bd423b605872e4269dfb2b353ededbea8290b7a
joereinhardt <joachim.reinhardt@fu-berlin.de>
2018-04-24 00:42:00 +0700

    add examples, discussion sections

--- vignettes/BayesianWilcoxTest.R
+++ vignettes/BayesianWilcoxTest.R
@@ -4,6 +4,21 @@ knitr::opts_chunk$set(
   comment = "#>"
 )
 
+## ---- message = FALSE----------------------------------------------------
+require(BayesianFirstAid)
+
+## ------------------------------------------------------------------------
+#Setting up example data
+data <- c(68, 24)
+#Classical test
+binom.test(data, p = 3/4, alternative = "l")
+
+## ---- fig.width = 5, fig.height = 5--------------------------------------
+#Bayesian Alternative
+bayesBinom <- bayes.binom.test(data, p = 3/4)
+print(bayesBinom)
+plot(bayesBinom)
+
 ## ------------------------------------------------------------------------
 pairDiff_means <- function(N) {
   #create x and y so that x > y
@@ -24,4 +39,33 @@ for (i in 2:500) {
   means <- c(means, pairDiff_means(i))
 }
 plot(means, type = "l", ylab = "mu_max",  xlab = "N")
+
+## ----graphPaired, echo=FALSE, out.width = '55%'--------------------------
+knitr::include_graphics("oneSampleWilcoxDiagram.svg")
+
+## ----graphTwo, echo=FALSE, out.width = '75%'-----------------------------
+knitr::include_graphics("twoSampleWilcoxDiagram.svg")
+
+## ----eval = FALSE--------------------------------------------------------
+#  require(devtools)
+#  install_github("joereinhardt/BayesianFirstAid-Wilcoxon")
+
+## ------------------------------------------------------------------------
+#Copied from the wilcox.test help file:
+# Hamilton depression scale factor measurements in 9 patients with
+#  mixed anxiety and depression, taken at the first (x) and second
+#  (y) visit after initiation of a therapy (administration of a
+#  tranquilizer).
+x <- c(1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30)
+y <- c(0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29)
+wilcox.test(x, y, paired = TRUE, alternative = "greater")
+
+## ------------------------------------------------------------------------
+require(bayesWilcoxTest)
+hamiltonBayesWilcox <- bayes.wilcox.test(x, y, paired = TRUE,
+                                         alternative = "greater")
+#Print out concise information on the test
+print(hamiltonBayesWilcox)
+#Show a more detailed Summary
+summary(hamiltonBayesWilcox)
 
