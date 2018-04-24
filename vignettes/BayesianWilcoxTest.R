9afd223908ae61f232f75530adec12e1b8544c5e
joereinhardt <joachim.reinhardt@fu-berlin.de>
2018-04-24 09:23:30 +0700

    add functionality section

--- vignettes/BayesianWilcoxTest.R
+++ vignettes/BayesianWilcoxTest.R
@@ -60,12 +60,51 @@ x <- c(1.83,  0.50,  1.62,  2.48, 1.68, 1.88, 1.55, 3.06, 1.30)
 y <- c(0.878, 0.647, 0.598, 2.05, 1.06, 1.29, 1.06, 3.14, 1.29)
 wilcox.test(x, y, paired = TRUE, alternative = "greater")
 
-## ------------------------------------------------------------------------
+## ---- fig.width = 5------------------------------------------------------
 require(bayesWilcoxTest)
 hamiltonBayesWilcox <- bayes.wilcox.test(x, y, paired = TRUE,
                                          alternative = "greater")
+
 #Print out concise information on the test
 print(hamiltonBayesWilcox)
-#Show a more detailed Summary
+
+#Show a more detailed summary
 summary(hamiltonBayesWilcox)
+
+#Visual Inspection of the posterior
+plot(hamiltonBayesWilcox)
+
+#MCMC diagnostics
+diagnostics.bayes_paired_wilcox_test(hamiltonBayesWilcox)
+
+#Obtain model code in order to modify as needed
+model.code.bayes_paired_wilcox_test(hamiltonBayesWilcox)
+
+## ------------------------------------------------------------------------
+#Copied from the wilcox.test help file:
+# Permeability constants of the human chorioamnion (a placental
+#  membrane) at term (x) and between 12 to 26 weeks gestational
+#  age (y).  The alternative of interest is greater permeability
+#  of the human chorioamnion for the term pregnancy.
+x <- c(0.80, 0.83, 1.89, 1.04, 1.45, 1.38, 1.91, 1.64, 0.73, 1.46)
+y <- c(1.15, 0.88, 0.90, 0.74, 1.21)
+wilcox.test(x, y, alternative = "g")
+
+## ---- fig.width = 5, fig.height = 5--------------------------------------
+membraneBayesWilcox <- bayes.wilcox.test(x, y, alternative = "g")
+
+#Print out concise information on the test
+print(membraneBayesWilcox)
+
+#Show a more detailed summary
+summary(membraneBayesWilcox)
+
+#Visual Inspection of the posterior
+plot(membraneBayesWilcox)
+
+#MCMC diagnostics
+diagnostics.bayes_two_sample_wilcox_test(membraneBayesWilcox)
+
+#Obtain model code in order to modify as needed
+model.code.bayes_two_sample_wilcox_test(membraneBayesWilcox)
 
